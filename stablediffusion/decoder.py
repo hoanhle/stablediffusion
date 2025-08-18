c612afd20ae9df5a4282c36cc22d950a3c096c35
Hoanh Le <hoanhle100100@gmail.com>
2025-08-18 19:43:11 +0700

    add decoder

---
+++ stablediffusion/decoder.py
@@ -0,0 +1,103 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+from attention import SelfAttention
+
+
+class VAE_AttentionBlock(nn.Module):
+    def __init__(self, channels: int):
+        super().__init__()
+        self.groupnorm = nn.GroupNorm(32, channels)
+        self.attention = SelfAttention(1, channels)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        residue = x
+
+        x = self.groupnorm(x)
+
+        b, c, h, w = x.shape
+
+        # (B, C, H, W) -> (B, C, H*W)
+        x = x.reshape(b, c, h * w)           
+
+        # (B, C, H*W) -> (B, H*W, C) 
+        x = x.transpose(-1, -2)
+
+        x = self.attention(x)
+
+        # (B, H*W, C) -> (B, C, H*W)
+        x = x.transpose(-1, -2)
+        x = x.reshape(b, c, h, w)     
+
+        x = x + residue
+        return x
+
+
+
+class VAE_ResidualBlock(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int):
+        super().__init__()
+        self.groupnorm_1 = nn.GroupNorm(32, in_channels)
+        self.conv_1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+        self.groupnorm_2 = nn.GroupNorm(32, out_channels)
+        self.conv_2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
+
+        if in_channels == out_channels:
+            self.residual = nn.Identity()
+        else:
+            self.residual = nn.Conv2d(in_channels, out_channels, kernel_size=1, padding=0)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        residue = x
+        x = self.groupnorm_1(x)
+        x = F.silu(x)
+        x = self.conv_1(x)
+        x = self.groupnorm_2(x)
+        x = F.silu(x)
+        x = self.conv_2(x)
+
+        x = x + self.residual(residue)
+        return x
+
+
+class VAE_Decoder(nn.Sequential):
+    def __init__(self):
+        super().__init__(
+            nn.Conv2d(4, 4, kernel_size=1, padding=0),
+            nn.Conv2d(4, 512, kernel_size=3, padding=1),
+            VAE_ResidualBlock(512, 512),
+            VAE_AttentionBlock(512),
+            VAE_ResidualBlock(512, 512),
+            VAE_ResidualBlock(512, 512),
+            VAE_ResidualBlock(512, 512),
+            VAE_ResidualBlock(512, 512),
+            nn.Upsample(scale_factor = 2),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            VAE_ResidualBlock(512, 512),
+            VAE_ResidualBlock(512, 512),
+            VAE_ResidualBlock(512, 512),
+            nn.Upsample(scale_factor = 2),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            VAE_ResidualBlock(512, 256),
+            VAE_ResidualBlock(256, 256),
+            VAE_ResidualBlock(256, 256),
+            nn.Upsample(scale_factor = 2),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            VAE_ResidualBlock(256, 128),
+            VAE_ResidualBlock(128, 128),
+            VAE_ResidualBlock(128, 128),
+            nn.GroupNorm(32, 128),
+            nn.SiLU(),
+            nn.Conv2d(128, 3, kernel_size=3, padding=1)
+        )
+    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # x: batch_size, 4, height / 8, width / 8
+        
+        # reverse the scaling of the encoder
+        x /= 0.18215
+
+        for module in self:
+            x = module(x)
+        
+        return x
